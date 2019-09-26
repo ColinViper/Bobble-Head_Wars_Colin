1554217cf9a0cb3a01c4d0757ce3ef1e6ce2662d
Colin Labelle <colinlabelle777@gmail.com>
2019-09-26 17:26:06 -0400

    BobbleHead Wars chpt 5, 3

--- Assets/Scripts/Alien.cs
+++ Assets/Scripts/Alien.cs
@@ -7,7 +7,8 @@ public class Alien : MonoBehaviour
 {
 
     public Transform target;
-    private NavMeshAgent agent;
+    private NavMeshAgent agent;    public float navigationUpdate;
+    private float navigationTime = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,7 +20,12 @@ public class Alien : MonoBehaviour
     {
         if (target != null)
         {
-            agent.destination = target.position;
+            navigationTime += Time.deltaTime;
+            if (navigationTime > navigationUpdate)
+            {
+                agent.destination = target.position;
+                navigationTime = 0;
+            }
         }
     }
 }
